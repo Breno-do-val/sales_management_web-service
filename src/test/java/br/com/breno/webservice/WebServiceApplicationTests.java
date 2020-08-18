119b3a6e0b7116e594921718c6260f9120f2e296
breno-do-val <brenorvale@gmail.com>
2020-08-17 23:54:19 -0300

    project created

---
+++ src/test/java/br/com/breno/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.breno.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
