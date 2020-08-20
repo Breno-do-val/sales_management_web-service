f00808f8e24df8a8da852e54a5a193f7e4e9d0f4
breno-do-val <brenorvale@gmail.com>
2020-08-19 23:51:54 -0300

    Order and Instant

--- src/main/java/br/com/breno/webservice/entities/User.java
+++ src/main/java/br/com/breno/webservice/entities/User.java
@@ -1,13 +1,18 @@
 package br.com.breno.webservice.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -18,6 +23,13 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	/*
+	 * Association - A client can have one or more orders
+	 * Collections must be initialized and provide only Get method
+	 * */
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -69,6 +81,10 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
