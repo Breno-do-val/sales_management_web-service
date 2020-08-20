24c01561ad2c34f93227b387ea599724cb35c54a
breno-do-val <brenorvale@gmail.com>
2020-08-19 23:33:01 -0300

    Service layer and component registration

--- src/main/java/br/com/breno/webservice/repositories/UserRepository.java
+++ src/main/java/br/com/breno/webservice/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.breno.webservice.entities.User;
 
+/*
+ * It is not necessary to put the annotation @Repository because the interface extends JpaRepository
+ * */
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/br/com/breno/webservice/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.breno.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.breno.webservice.entities.User;
+import br.com.breno.webservice.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
