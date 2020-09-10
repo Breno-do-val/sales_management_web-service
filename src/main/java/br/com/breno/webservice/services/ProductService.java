0001d259b6600b3e4e2b07382b1caef78426dc98
breno-do-val <brenorvale@gmail.com>
2020-09-09 23:44:59 -0300

    product entity

---
+++ src/main/java/br/com/breno/webservice/services/ProductService.java
@@ -0,0 +1,26 @@
+package br.com.breno.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.breno.webservice.entities.Product;
+import br.com.breno.webservice.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
