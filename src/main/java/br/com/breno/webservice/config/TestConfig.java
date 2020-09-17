97d5039446542da8680bbb34435b2893fa3b8eae
breno-do-val <brenorvale@gmail.com>
2020-09-17 18:49:11 -0300

    CRUD concluded

--- src/main/java/br/com/breno/webservice/config/TestConfig.java
+++ src/main/java/br/com/breno/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.breno.webservice.entities.Category;
 import br.com.breno.webservice.entities.Order;
 import br.com.breno.webservice.entities.OrderItem;
+import br.com.breno.webservice.entities.Payment;
 import br.com.breno.webservice.entities.Product;
 import br.com.breno.webservice.entities.User;
 import br.com.breno.webservice.entities.enums.OrderStatus;
@@ -86,5 +87,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/br/com/breno/webservice/entities/OrderItem.java
+++ src/main/java/br/com/breno/webservice/entities/OrderItem.java
@@ -66,6 +66,9 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
